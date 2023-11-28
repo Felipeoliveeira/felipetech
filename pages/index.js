e25547f169397915a2380e2dd3d484f25d9dc6ef
Felipe José Santos de Oliveira <felipeoliveira.aju79@gmail.com>
2023-11-28 23:44:26 +0000

    Adicionando os arquivos de manifesto

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Novos commits</h1>
+    return <h1>Olá pessoal, estou aprendendo Git com Filipe Deschamps, no curso.dev ! E está sendo incrível aprender a usar essa ferramenta de versionamento de códigos!</h1>
 }
 
-export default Home();
+export default Home;
