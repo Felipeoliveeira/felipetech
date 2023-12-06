ae349f254ae0de3e1b7b0fe87eaf2d5d9d4ef2ac
Felipe José Santos de Oliveira <felipeoliveira.aju79@gmail.com>
2023-12-06 23:58:46 +0000

    new commit

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Olá, me chamo Felipe Oliveira e estou contente em fazer o curso.dev, com Filipe Deschamps! Estou animado para continuar aprendendo!</h1>
+    return <h1>Olá, me chamo Felipe Oliveira e estou contente em fazer o curso.dev, com Filipe Deschamps! Estou animado para continuar aprendendo !</h1>
 }
 
 export default Home;
