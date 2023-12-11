3533ce0425a76213e14d25835748de0ed85d2332
Felipe José Santos de Oliveira <felipeoliveira.aju79@gmail.com>
2023-12-11 22:28:52 +0000

    adicionando scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Olá, me chamo Felipe Oliveira e estou contente em fazer o curso.dev com Filipe Deschamps! Estou animado para continuar aprendendo !</h1>
+  return (
+    <h1>
+      Olá, me chamo Felipe Oliveira e estou aqui para dar o meu melhor com este
+      novo projeto do tabnews.
+    </h1>
+  );
 }
 
 export default Home;
