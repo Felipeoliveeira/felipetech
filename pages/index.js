31c0bdb8f918b7714155ec9436311d6afc1a55e9
Felipe Oliveira <felipeoliveira.aju79@gmail.com>
2024-01-03 19:38:52 -0300

    tester

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
 function funcao(){
-  return <h1> Chamo-me Felipe Oliveira e estou animado com o curso.dev</h1>
+  return <h1> Chamo me Felipe Oliveira e estou animado com o curso.dev</h1>
 }
 export default funcao
